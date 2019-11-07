cc0ea50ea565c1cc80d3a06f8b436f7f0cf841fd
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 17:54:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,5 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect {|cheese_types|
-    return cheese_types
-  }
+  i=0
 end
