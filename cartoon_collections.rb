cc2b3ffe85c1020845bcf624ceb830c29681b295
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 17:21:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |names,index|
+    puts "#{index} #{names}"
+
+  }
+
 end
 
 def summon_captain_planet# code an argument here
