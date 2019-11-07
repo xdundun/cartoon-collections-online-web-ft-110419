a87b7b97172cdc262c21538b2483adf9bb332e0b
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 17:37:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,10 @@ def summon_captain_planet(array)
   return elements
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls?(array)
+  array.any? {|elem|
+    elem.size > 4
+  }
 end
 
 def find_the_cheese# code an argument here
